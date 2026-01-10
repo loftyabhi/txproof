d45a3f1addafaec1de30959f9734b8bf362d740f
loftyabhi <117755801+loftyabhi@users.noreply.github.com>
2026-01-10 13:00:26 +0530

    Remove disallow rule for '/_next' in robots.ts

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api', '/print', '/_next'],
+        disallow: ['/api', '/print'],
       },
     ],
     sitemap: 'https://chainreceipt.vercel.app/sitemap.xml',
