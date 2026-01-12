8204d9f76afc58b804b995a7bcb039d322a96828
loftyabhi <abhisekshaw0206@gmail.com>
2026-01-12 20:49:34 +0530

    minor mistake corrected

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
+                hostname: '*.aads.com', // [Enterprise] Add alternative domain
+            },
+            {
+                protocol: 'https',
                 hostname: '*.ad-maven.com',
             },
             {
